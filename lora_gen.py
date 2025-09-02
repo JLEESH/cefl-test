98badb14a1917bfecb6d9f39f658e053c39cc53a
JLEESH <jimmyleesh@yahoo.co.kr>
2025-09-01 21:10:16 -0400

    microupdate.

--- lora_gen.py
+++ lora_gen.py
@@ -8,6 +8,8 @@ class EMBGPT2LoRAGen(nn.Module):
         
         ```
         config={
+            gpt2,
+            gpt2_tokenizer, # (note aliases)
             emb_types, # (no. of layer_index * weight_type combinations)
             emb_output_dim, # (=gpt2.transformer.h[0] input dim.)
             lora_gen_in_dim, # (=gpt2.transformer.h[11] output dim.)
@@ -52,12 +54,13 @@ class EMBGPT2LoRAGen(nn.Module):
         self.lora_out_dim = config.lora_out_dim
     
     def forward(self, x):
+    #def forward(self, layer_index, weight_type, data_emb=None):
+        #x = self._make_input(layer_index, weight_type, data_emb=data_emb)
+        
         # pass embeddings thru model
-        #x = self.gpt2(x)
         for layer_index in range(len(self.gpt2.transformer.h)):
-            x = self.gpt2.transformer.h[layer_index](x)[0] # TODO: fix leaving out (ln_f)
-        x = self.gpt2.transformer.ln_f(x) # fixed
-        # vs self.gpt2.lm_head(x)
+            x = self.gpt2.transformer.h[layer_index](x)[0]
+        x = self.gpt2.transformer.ln_f(x)
         x = self.o2l(x)
         
         # reshape to lora dimensions
@@ -76,8 +79,11 @@ class EMBGPT2LoRAGen(nn.Module):
         x = self.emb(layer_index * weight_type)
         if data_emb is not None:
             x = torch.cat((x, data_emb))
-        x = x.reshape((1, 1, 768))
+        x = x.reshape((1, 1, x.shape[-1]))
         return x
+    #_make_input = make_input
+    #make_input = _make_input
+
 # EMB-Hypernetwork-LoRAGenerator <- EHLG
 EHLG_GPT2 = EMBGPT2LoRAGen
 EHLG = EHLG_GPT2
@@ -94,20 +100,24 @@ def lora_gen_test():
     config.gpt2 = AutoModelForCausalLM.from_pretrained('gpt2')
     config.gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
     config.emb_types = nLayers * nWeightTypes # i.e. EHLG does not currently know how the embeddings came about
-    DIM = 768
-    config.emb_output_dim = 768
     
-    config.lora_gen_in_dim = 768 # (=gpt2 output dim.)
-    config.lora_gen_out_dim = 6144 * 2 # (=A_size + B_size)
+    EMB_DIM = 768
+    LORA_DIM = 768
+    config.emb_output_dim = EMB_DIM
+    
+    config.lora_gen_in_dim = EMB_DIM # (=gpt2 output dim.)
+    config.lora_gen_out_dim = 6144 * 2 # (=A_size + B_size) # TODO: fix A_size calculation redundancies
     config.lora_rank = 8
-    config.lora_in_dim = 768
+    config.lora_in_dim = LORA_DIM
     config.lora_out_dim = config.lora_in_dim
     
     eg_model = EHLG(config)
     return eg_model
 
 def lora_gen_test_p2(eg_model):
-    x = eg_model.make_input(torch.tensor(1), torch.tensor(1)) # maybe move everything to forward()?
+    layer_index = 1
+    weight_type = 1
+    x = eg_model.make_input(torch.tensor(layer_index), torch.tensor(weight_type)) # TODO: maybe move everything to forward()?
     output = eg_model(x)
     return output
 
