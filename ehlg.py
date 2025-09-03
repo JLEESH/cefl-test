978febe0f371a823e0a7e966c56c64f93d925a89
JLEESH <jimmyleesh@yahoo.co.kr>
2025-09-03 02:39:20 -0400

    Removed old functions from ehlg.py.

--- ehlg.py
+++ ehlg.py
@@ -47,18 +47,9 @@ class EMBGPT2LoRAGen(nn.Module):
         x = self._inner_forward(x)
         ab_dict = self.reshape_output(x, layer_indices, weight_types)
         return ab_dict
-        
-        # # TODO: consider doing away with making the input and output separately
-        # # pass embeddings thru model
-        # for layer_index in range(len(self.gpt2.transformer.h)):
-        #     x = self.gpt2.transformer.h[layer_index](x)[0]
-        # x = self.gpt2.transformer.ln_f(x)
-        # x = self.o2l(x)
-        
-        # return x
     
     def _inner_forward(self, x):
-        # pass embeddings thru model
+        # pass embeddings to hypernetwork and the subsequent output to the lora generator
         for layer_index in range(len(self.gpt2.transformer.h)):
             x = self.gpt2.transformer.h[layer_index](x)[0]
         x = self.gpt2.transformer.ln_f(x)
@@ -165,81 +156,5 @@ def main():
     #print(ehlg_test_bare())
     ehlg_test()
 
-def lora_gen_test():
-    # TODO: to turn the following into a class function?
-    from transformers import AutoTokenizer, AutoModelForCausalLM
-    from datasets import load_dataset
-    from types import SimpleNamespace
-    
-    nLayers = 12
-    nWeightTypes = 2
-    
-    config = SimpleNamespace()
-    config.gpt2 = AutoModelForCausalLM.from_pretrained('gpt2')
-    config.gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
-    config.nlayers = nLayers
-    config.ntypes = nWeightTypes
-    
-    EMB_DIM = 768 # same as the (wte)/embedding dimensions of the hypernetwork in EHLG
-    LORA_DIM = 3200 # should be the same as OLM weight dimensions (ehlg.o2l could be reduced in size)
-    LORA_RANK = 8
-    config.emb_output_dim = EMB_DIM
-    
-    config.lora_gen_in_dim = EMB_DIM # (=gpt2 output dim.)
-    #config.lora_gen_out_dim = 6144 * 2 # (=A_size + B_size) # TODO: fix A_size calculation redundancies
-    config.lora_gen_out_dim = LORA_DIM * LORA_RANK * 2 # (no. of params grows by almost x20... to fix)
-    config.lora_rank = LORA_RANK
-    config.lora_in_dim = LORA_DIM
-    config.lora_out_dim = config.lora_in_dim
-    
-    #eg_model = EHLG(config)
-    #return eg_model
-    
-    ehlg_model = EHLG(config)
-    return ehlg_model
-
-def lora_gen_test_p2(ehlg_model, layer_indices, weight_types):
-    # TODO: likewise, perhaps turn it into a class function
-    
-    #eg_model = ehlg_model
-    #layer_index = 1
-    #weight_type = 1
-    #x = eg_model.make_input(torch.tensor(layer_index), torch.tensor(weight_type))
-    #output = eg_model(x)
-    
-    ehlg = ehlg_model
-    x = ehlg.make_input_batch(layer_indices, weight_types)
-    #output = ehlg(x)
-    output = ehlg.forward_old(x)
-    output_ab_dict = ehlg.reshape_output(output, layer_indices, weight_types)
-    return output_ab_dict
-
-def old_main():
-    # TODO: Instantiation of EHLG using default settings as described in tests
-    
-    # test implementation
-    print("running some basic tests...")
-    print("creating model (ehlg)...")
-    eg_model = lora_gen_test() # should be a class function (constructor? configuration method?)
-    print("ehlg(=eg_model) created.")
-    
-    # print("testing forward pass with li=1 and wt=1...")
-    # output = lora_gen_test_p2(eg_model)
-    # print("output from eg_model...")
-    # #print(f"output: {output}")
-    # print("A:%s\n B:%s" % (output[0].shape, output[1].shape))
-    
-    layer_indices = [0, 1, 2, 4, 8]
-    weight_types = [[0, 1], [0, 1], [0], [1], [0, 1]]
-    print(f"testing forward pass with li={layer_indices} and wt={weight_types}...")
-    
-    # likewise, perhaps the testing function below should be made part of the model
-    output_ab_dict = lora_gen_test_p2(eg_model, layer_indices, weight_types)
-    
-    print("output from eg_model...")
-    print(f"output: {output_ab_dict}")
-
-
 if __name__ == '__main__':
     main()
-    #old_main()
